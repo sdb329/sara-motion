89f79659ec3600edde77f86f382f5cd150021eb8
sdb329 <sdb329@drexel.edu>
2021-10-03 18:44:46 -0400

    animation done

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -22,5 +22,13 @@ mainTL
 .from("#hero #l1",{duration:2.5,y:"+=400", alpha:0, rotation:360, ease:"back"},"-=1")
 .from("#hero #l2",{duration:2.5,y:"+=900", alpha:0, rotation:360, ease:"back"},"-=2.5")
 
+
+.from("#content h2",{duration:2.5,scale:0, alpha:0, rotation:360, ease:"back"}, "-=2")
+.from("#content #boxes-grid",{duration:4,alpha:0}, "-=4")
+.from("#content #imagec",{duration:4,alpha:0, x:"+=400"}, "<")
+.from("#content p",{duration:6, x:"+=150", alpha:0, stagger:0.4, ease:"back"}, "-=3")
+
+
+
 ;
 
