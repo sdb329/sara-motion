875a04c42dfcc11b8fee04ca84d94405e7a49084
sdb329 <sdb329@drexel.edu>
2021-11-21 19:25:30 -0500

    custom-ease

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -9,7 +9,8 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { Flip } from "gsap/Flip";
 import { MotionPathHelper } from "gsap/MotionPathHelper";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
-gsap.registerPlugin(GSDevTools, MorphSVGPlugin, DrawSVGPlugin, Flip, MotionPathHelper, MotionPathPlugin);
+import { CustomEase } from "gsap/CustomEase";
+gsap.registerPlugin(GSDevTools, MorphSVGPlugin, DrawSVGPlugin, Flip, MotionPathHelper, MotionPathPlugin, CustomEase);
 
 // // const 
 // // var
@@ -18,8 +19,19 @@ gsap.registerPlugin(GSDevTools, MorphSVGPlugin, DrawSVGPlugin, Flip, MotionPathH
 
 const mainTL = gsap.timeline();
 
+
+
 function snowflakeOne() {
     const tl = gsap.timeline();
+    // tl.set("#drawsvg-snowflake", {
+    //     alpha:0
+    //  });
+    tl.set("#extra-line", {
+        alpha: 0
+    });
+    tl.set("#stroke-bounce", {
+        alpha: 1
+    });
 
     tl.from(".snowflake-1", {
         drawSVG: 0,
@@ -154,25 +166,82 @@ function snowflakeFalling() {
 
     const tl = gsap.timeline();
 
-    tl.to("#snowflakes", {
+    tl.to(".snowflake-fall", {
         y: "+=1000",
         duration: 5,
-        stagger: 5
+        stagger: 0.2
     });
     
    
     return tl;
 }
 
+function snowflakeBouncingLine() {
+
+    const tl = gsap.timeline();
+
+    tl.set("#stroke-bounce", {
+        alpha: 0
+    }, "stokeSame");
+
+    // tl.set("#extra-line", {
+    //     alpha: 1
+    // }, "stokeSame");
+
+    tl.to("#snowflake1", {
+        y: "+=1000",
+        duration: 5,
+        stagger: 0.2
+    }, "stokeSame");
+
+    // tl.to("#extra-line", {
+    //     duration: .2,
+    //     drawSVG: "0%"
+    // }, "stokeSame");
+
+    
+
+
+    
+
+   
+    return tl;
+}
+
+function snowflakeDrawSVG() {
+
+    const tl = gsap.timeline();
+
+    tl.from("#drawsvg-snowflake", {
+        alpha:0,
+        duration: .01
+     });
+ 
+     tl.fromTo("#drawsvg-snowflake", {
+ 
+         duration: 2,
+         drawSVG: "0% 8.5%"
+     },
+     {
+         duration: 2, ease: "slowMiddle", drawSVG: "90% 100%"});
+     
+    
+   
+    return tl;
+}
+
+CustomEase.create("slowMiddle", "M0,0 C0.13,0.428 0.111,0.394 0.462,0.534 0.764,0.654 0.876,0.7 1,1 ");
 
 mainTL
     .add(snowflakeOne())
-    .add(snowflakeTwo(), "-=7.7")
-    .add(snowflakeThree(), "-=7.7")
     .add(snowflakeFour(), "-=7.7")
+    .add(snowflakeTwo(), "-=7.7")
     .add(snowflakeFive(), "-=7.7")
+    .add(snowflakeThree(), "-=7.7")
     .add(snowflakeSix(), "-=7.7")
     .add(snowflakeFalling(), "-=5")
+    .add(snowflakeBouncingLine(), "-=4.68")
+    .add(snowflakeDrawSVG(), "-=5")
 
 ;
 
@@ -211,3 +280,4 @@ DrawSVGPlugin.create();
 Flip.create();
 MotionPathHelper.create();
 MotionPathPlugin.create();
+CustomEase.create();
